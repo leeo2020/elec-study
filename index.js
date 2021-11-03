058cdde53ecc827bacff3638b9d7aebfe2683bd5
leeo2020 <3145875098@qq.com>
2021-11-03 22:13:28 +0800

    'init'

---
+++ index.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron')
+
+let win = null
+app.on('ready', () => {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
+    }
+  })
+  win.loadFile('index.html')
+  app.on('closed', () => {
+    app.exit()
+    win = null
+  })
+})
